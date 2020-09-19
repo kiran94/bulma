4455c040e8febd6ac89684a90420de5214222b32
Kiran94 <kiran_patel94@hotmail.com>
2020-09-19 12:38:18 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as desc:
+    long_description = desc.read()
+
+with open("requirements.txt", 'r') as require:
+    packages = require.readlines()
+    packages = [p.strip('\n') for p in packages]
+
+setuptools.setup(
+    name="bulma", # Replace with your own username
+    version="0.0.2",
+    author="Kiran Patel",
+    description="Wrapper for Vegeta Load Testing tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kiran94/bulma",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=packages,
+    python_requires='>=3.6',
+)
